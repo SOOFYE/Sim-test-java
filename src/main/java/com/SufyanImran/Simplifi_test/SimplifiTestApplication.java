7010ba824ea20d913ca6762dd9a1056599bdb639
sufyan imran <syedsufyan36@gmail.com>
2025-07-15 23:00:36 +0500

    init commit

---
+++ src/main/java/com/SufyanImran/Simplifi_test/SimplifiTestApplication.java
@@ -0,0 +1,13 @@
+package com.SufyanImran.Simplifi_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplifiTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplifiTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SufyanImran/Simplifi_test/SimplifiTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SufyanImran.Simplifi_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplifiTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
